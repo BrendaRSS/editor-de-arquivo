b06260d06d3934feee805ecd6013a7b2db096647
Brenda Rayane <brenda.rayane.0018@gmail.com>
2025-03-18 13:54:56 -0300

    feat: get users tests

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -71,3 +71,43 @@ describe('POST /users', () => {
           }));
       });
 });
+
+describe("GET  users", () => {
+    const validUser = {
+        nome: faker.lorem.word(),
+        email: faker.internet.email(),
+        idade: faker.number.int(),
+    };
+
+    it("should return status 200 and users when service succeeds", async () => {
+        await server.post("/users").send(validUser);
+        const response = await server.get("/users");
+
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toEqual(expect.arrayContaining([
+            expect.objectContaining({ 
+                id: expect.any(Number),
+                email: validUser.email,
+                nome: validUser.nome,
+                idade: validUser.idade})
+            ]))
+        })
+
+    it("should return status 404 if id is invalid", async () => {
+        await server.post("/users").send(validUser);
+        const response = await server.get("/users/0");
+        expect(response.status).toBe(httpStatus.NOT_FOUND);
+    })
+
+    it("should return status 200 and user when id is valid", async () => {
+        const userCreate = await server.post("/users").send(validUser);
+        const response = await server.get(`/users/${userCreate.body.id}`);
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toEqual(expect.objectContaining({
+            id: userCreate.body.id,
+            email: validUser.email,
+            nome: validUser.nome,
+            idade: validUser.idade
+        }))
+    });
+})
